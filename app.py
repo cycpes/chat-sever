09ad4c24d259f860413c36fd123575e13f2b8d8e
cycpes <90095227+cycpes@users.noreply.github.com>
2021-10-13 21:45:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((SERVER_HOST, SERVER_PORT))
 # listen for upcoming connections
 s.listen(5)
-print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
 
 def listen_for_client(cs):
     """
@@ -27,10 +26,9 @@ def listen_for_client(cs):
         try:
             # keep listening for a message from `cs` socket
             msg = cs.recv(1024).decode()
-        except Exception as e:
+        except Exception:
             # client no longer connected
             # remove it from the set
-            print(f"[!] Error {e}")
             client_sockets.remove(cs)
         else:
             # if we received a message, replace the <SEP> 
@@ -45,7 +43,6 @@ def listen_for_client(cs):
 while True:
     # we keep listening for new connections all the time
     client_socket, client_address = s.accept()
-    print(f"[+] {client_address} connected.")
     # add the new connected client to connected sockets
     client_sockets.add(client_socket)
     # start a new thread that listens for each client's messages
